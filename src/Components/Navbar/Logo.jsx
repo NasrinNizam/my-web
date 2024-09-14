a2b1308e989f6193e8d790df9c488ee06715b4e8
nasrin sultana <nasrinbinteenizam@gmail.com>
2024-09-14 12:50:49 +0600

    change in logo

--- src/Components/Navbar/Logo.jsx
+++ src/Components/Navbar/Logo.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 export const Logo = () => {
   return (
     <>
-    <Link to='/' className="animation hover:scale-105 transition-all">
-        <span class="a">N</span>
+    <Link to='/' className="animation hover:scale-105 transition-all md:text-[30px] text-[20px] ">
+        <span className="a">N</span>
         <span>a</span>
-        <span class="a">s</span>
+        <span className="a">s</span>
         <span>r</span>
-        <span class="a">i</span>
+        <span className="a">i</span>
         <span>n</span>
     </Link>
     </>
